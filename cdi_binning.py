d8cfcfce73913f00eeaea65daa1e73871c303cac
emcguirk <39199936+emcguirk@users.noreply.github.com>
2023-04-24 22:03:01 -0400

    Finish binning script

--- cdi_binning.py
+++ cdi_binning.py
@@ -1,10 +1,8 @@
 import pandas as pd
-import numpy as np
 
 cdi = pd.read_csv('CDI_with_pop_centers.csv')
 cdi = cdi[[
     'YearStart',
-    'YearEnd',
     'LocationAbbr',
     'LocationDesc',
     'Topic',
@@ -15,8 +13,20 @@ cdi = cdi[[
     'Centroid',
 ]]
 
+cdi.rename(columns={'YearStart': 'Year'}, inplace=True)
+
 topics = cdi['Topic'].value_counts().index.to_list()
 
 topic_dfs = [cdi[cdi['Topic'] == x] for x in topics]
-
+for df in topic_dfs:
+    bin_labels = [
+        'very low',
+        'low',
+        'middle',
+        'high',
+        'very high'
+    ]
+    name = pd.unique(df['Topic'])[0]
+    df['Category'] = pd.qcut(x=df['DataValue'], q=5, labels=bin_labels)
+    df.to_csv(f'./processed_csvs/{name.lower()}.csv', index=False)
 #%%
